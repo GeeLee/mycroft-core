ff50dcd9390b698ddbd1f57856677c4be40c347b
Michael Nguyen <ppnguyen91@gmail.com>
2018-07-06 12:56:56 -0500

    added asynchronous request to mimic2 backend to break up audio into chunks

--- mycroft/tts/mimic2_tts.py
+++ mycroft/tts/mimic2_tts.py
@@ -18,8 +18,97 @@ from mycroft.tts.remote_tts import RemoteTTS
 from mycroft.configuration import Configuration
 from mycroft.util.log import LOG
 from mycroft.util import play_wav
+from requests_futures.sessions import FuturesSession
 from urllib import parse
 import requests
+import math
+
+
+def break_chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield " ".join(l[i:i + n])
+
+
+def split_by_chunk_size(text, chunk_size):
+    """split text into word chunks by chunk_size size
+
+    Args:
+        text (str): text to split
+        chunk_size (int): chunk size
+
+    Returns:
+        list: list of text chunks
+    """
+    text_list = text.split()
+
+    if len(text_list) <= chunk_size:
+        return [text]
+
+    if chunk_size < len(text_list) < (chunk_size * 2):
+        return list(break_chunks(
+            text_list,
+            int(math.ceil(len(text_list) / 2))
+        ))
+    elif (chunk_size * 2) < len(text_list) < (chunk_size * 3):
+        return list(break_chunks(
+            text_list,
+            int(math.ceil(len(text_list) / 3))
+        ))
+    elif (chunk_size * 3) < len(text_list) < (chunk_size * 4):
+        return list(break_chunks(
+            text_list,
+            int(math.ceil(len(text_list) / 4))
+        ))
+    else:
+        return list(break_chunks(
+            text_list,
+            int(math.ceil(len(text_list) / 5))
+        ))
+
+
+def sentence_chunker(text, chunk_size, split_by_punc=True):
+    """split sentences into chunls. if split_by_punc is True,
+        sentences will be split into chunks by punctuations first
+        then those chunks will be split by chunk size
+
+    Args:
+        text (str): text to split
+        chunk_size (int): size of each chunk
+        split_by_punc (bool, optional): Defaults to True.
+
+    Returns:
+        list: list of text chunks
+    """
+    split_punctuations = [',', '.', '-', '?', '!', ':', ';']
+
+    text_list = text.split()
+    # if initial text is 1.3 times chunk size, no need to split
+    if len(text_list) <= chunk_size * 1.3:
+        return [text]
+
+    # split text by punctuations if split_by_punc set to true
+    # i.e "hello, world" -> ["hello", "world"]
+    punc_splits = None
+    if split_by_punc:
+        if len(text_list) >= chunk_size:
+            for punc in split_punctuations:
+                if punc in text:
+                    punc_splits = text.split(punc)
+                    break
+
+    # split text by chunk size
+    chunks = []
+    if punc_splits:
+        for sentence in punc_splits:
+            sentence = sentence.strip()
+            chunks += split_by_chunk_size(sentence, chunk_size)
+    # split text by chunk size
+    else:
+        chunks += split_by_chunk_size(text, chunk_size)
+
+    return chunks
+
 
 class Mimic2(RemoteTTS):
     PARAMS = {'accept': 'audio/wav'}
@@ -29,13 +118,26 @@ class Mimic2(RemoteTTS):
             lang, config, config['url'],
             config['api_path'], Mimic2Validator(self)
         )
-        LOG.info("mimic2config: " + str(config))
+        self.session = FuturesSession()
+        chunk_size = config.get('chunk_size')
+        self.chunk_size = \
+            chunk_size if chunk_size is not None else 10
 
     def _save(self, data):
+        """saves .wav files in tmp
+
+        Args:
+            data (byes): wav data
+        """
         with open(self.filename, 'wb') as f:
             f.write(data)
-    
+
     def _play(self, req):
+        """play wav file after saving to tmp
+
+        Args:
+            req (object): requests object
+        """
         if req.status_code == 200:
             self._save(req.content)
             play_wav(self.filename).communicate()
@@ -45,25 +147,47 @@ class Mimic2(RemoteTTS):
                 (req.status_code, req.reason, req.url))
 
     def build_request_params(self, sentence):
-        params = self.PARAMS.copy()
-        params['text'] = sentence.encode('utf-8')
-        return params
+        """RemoteTTS expects this method as abc.abstractmethod"""
+        pass
+
+    def _requests(self, chunks):
+        """create asynchronous request list
+
+        Args:
+            chunks (list): list of text to synthesize
+
+        Returns:
+            list: list of FutureSession objects
+        """
+        reqs = []
+        for chunk in chunks:
+            req_route = \
+                self.url + self.api_path + parse.quote(chunk)
+            reqs.append(self.session.get(req_route))
+        return reqs
 
     def execute(self, sentence, ident=None):
-        try:
-            req_route = \
-                self.url + self.api_path + parse.quote(sentence)
-            LOG.info(req_route)
-            req = requests.get(req_route)
-            self.begin_audio()
-            self._play(req)
-        except Exception as e:
-            LOG.error(e.message)
-        finally:
-            self.end_audio()
-        
+        """request and play mimic2 wav audio
+
+        Args:
+            sentence (str): sentence to synthesize from mimic2
+            ident (optional): Defaults to None.
+        """
+        chunks = sentence_chunker(sentence, self.chunk_size)
+        LOG.info(chunks)
+        for req in self._requests(chunks):
+            try:
+                res = req.result()
+                self.begin_audio()
+                self._play(res)
+            except Exception as e:
+                LOG.error(e)
+            finally:
+                self.end_audio()
+
 
 class Mimic2Validator(TTSValidator):
+
     def __init__(self, tts):
         super(Mimic2Validator, self).__init__(tts)
 
